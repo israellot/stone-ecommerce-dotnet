ccdc642f56512ca0b08dd9518d7aec62ce0a0ae8
Munir Wanis <fallen.angel.pp@gmail.com>
2015-12-17 18:33:14 -0200

    key

--- GatewayApiClient.Tests/BaseTests.cs
+++ GatewayApiClient.Tests/BaseTests.cs
@@ -8,6 +8,6 @@ namespace GatewayApiClient.Tests {
     public class BaseTests {
         
         // MerchantKey
-        public readonly Guid MerchantKey = Guid.Parse("8A2DD57F-1ED9-4153-B4CE-69683EFADAD5");
+        public readonly Guid MerchantKey = Guid.Parse("Merchant Key");
     }
 }
